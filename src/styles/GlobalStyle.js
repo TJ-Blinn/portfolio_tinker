b652d4cb3a5da43f8ef3d427aa7839dae1391d58
TJ <77421405+TJ-Blinn@users.noreply.github.com>
2022-02-28 21:04:31 -0500

    add root element variables for CSS in GlobalStyle

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,15 +1,32 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
+${"" /* define root element and specify vars to be used accross the page */}
+:root{
+  --primary-color: #007bff;
+  --seconday-color: #6c757d;
+  --background-dark-color: #10121a;
+  --border-color:#2e344e;
+  --background-light-color: #f1f1f1;
+  --white-color: #fff;
+  --font-light-color: #a4acc4;
+  --font-dark-color: #313131;
+  --font-dark-color-2:#151515;
+  
+}
+
+
   *{
       padding: 0;
       margin:0;
       box-sizing: border-box;
       font-size: 1.2rem;
+      list-style: none;
+      text-decoration: none;
   }
 
   body{
-    background-color: #333;
+    background-color: var(--background-dark-color);
   }
 `;
 export default GlobalStyle;

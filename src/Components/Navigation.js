e1849fe6b4dec7283cd84122063d0fa58fc55633
TJ <77421405+TJ-Blinn@users.noreply.github.com>
2022-02-28 22:15:35 -0500

    add Navigation component with react-router-dom

---
+++ src/Components/Navigation.js
@@ -0,0 +1,25 @@
+import React from "react";
+import styled from "styled-components";
+import { NavLink } from "react-router-dom";
+import avatar from "../img/avatar.jpg";
+
+function Navigation() {
+  return (
+    <NavigationStyled>
+      <div className="avatar">
+        <img src={avatar} alt="" />
+      </div>
+      <ul className="nav-items">
+        <li className="nav-item">
+          <NavLink to="/home" activeClassName="activeClass">
+            Home
+          </NavLink>
+        </li>
+      </ul>
+    </NavigationStyled>
+  );
+}
+
+const NavigationStyled = styled.nav``;
+
+export default Navigation;

7bd2235c3a23d78262b97eb91883de84398d5561
TJ <77421405+TJ-Blinn@users.noreply.github.com>
2022-03-01 16:58:32 -0500

    added MainContentStyled component and styling for main down center of page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,56 @@
+// import styled from "styled-components";
+import Sidebar from "./Components/Sidebar";
 import styled from "styled-components";
 
 function App() {
   return (
-    <AppStyled>
-      <h1>Styled Components</h1>
-    </AppStyled>
+    <div className="App">
+      <Sidebar />
+      <MainContentStyled>
+        <div className="lines">
+          <div className="line-1"></div>
+          <div className="line-2"></div>
+          <div className="line-3"></div>
+          <div className="line-4"></div>
+        </div>
+      </MainContentStyled>
+    </div>
   );
 }
 
-const AppStyled = styled.div`
-  background-color: red;
+const MainContentStyled = styled.main`
+  position: relative;
+  margin-left: 16.3rem;
+  min-height: 100vh;
+
+  .lines {
+    position: absolute;
+    min-height: 100vh;
+    width: 100%;
+    display: flex;
+    justify-content: space-evenly;
+
+    .line-1,
+    .line-2,
+    .line-3,
+    .line-4 {
+      width: 1px;
+      min-height: 100vh;
+      background-color: var(--border-color);
+    }
+  }
 `;
+
+// function App() {
+//   return (
+//     <AppStyled>
+//       <h1>Styled Components</h1>
+//     </AppStyled>
+//   );
+// }
+
+// const AppStyled = styled.div`
+//   background-color: red;
+// `;
 
 export default App;

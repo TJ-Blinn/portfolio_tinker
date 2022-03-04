856febedb9892675074531c1dd607ce9c3b2fbd5
TJ <77421405+TJ-Blinn@users.noreply.github.com>
2022-03-03 22:56:19 -0500

    adjusted styling of lines

--- src/App.js
+++ src/App.js
@@ -53,10 +53,12 @@ const MainContentStyled = styled.main`
 
   .lines {
     position: absolute;
-    min-height: 100vh;
+    min-height: 100%;
     width: 100%;
     display: flex;
     justify-content: space-evenly;
+    opacity: 0.4;
+    z-index: -1;
 
     .line-1,
     .line-2,

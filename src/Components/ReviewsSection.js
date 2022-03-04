af434881a263af848b0b90dd881644fd2e5a64db
TJ <77421405+TJ-Blinn@users.noreply.github.com>
2022-03-03 22:51:49 -0500

    add component for container of text (reviewitem)

---
+++ src/Components/ReviewsSection.js
@@ -0,0 +1,36 @@
+import React from "react";
+import styled from "styled-components";
+import { InnerLayout } from "../styles/layouts";
+import ReviewItem from "./ReviewItem";
+import Title from "./Title";
+
+function ReviewsSection() {
+  return (
+    <ReviewsStyled>
+      <Title title={"Reviews"} span={"Reviews"} />
+      <InnerLayout>
+        <div className="reviews">
+          <ReviewItem
+            text={
+              "Praesent sed cursus metus. Aliquam efficitur leo viverra, convallis justo eu, porta lectus. Nulla commodo commodo ligula vitae feugiat. Nullam id libero est."
+            }
+          />
+
+          <ReviewItem
+            text={
+              "Praesent sed cursus metus. Aliquam efficitur leo viverra, convallis justo eu, porta lectus. Nulla commodo commodo ligula vitae feugiat. Nullam id libero est. Maecenas pharetra diam in elit cursus molestie. Duis varius risus quis ullamcorper placerat. Vivamus et dignissim tellus. Proin dui lorem, efficitur in sollicitudin quis, pretium sed lacus. Donec sem justo, sollicitudin nec ullamcorper a, blandit vitae ex. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed sed maximus nunc. Praesent tristique laoreet lacus, sit amet convallis mi facilisis eu."
+            }
+          />
+        </div>
+      </InnerLayout>
+    </ReviewsStyled>
+  );
+}
+
+const ReviewsStyled = styled.section`
+  .reviews {
+    display: flex;
+  }
+`;
+
+export default ReviewsSection;

148ae2dd829aad4f5acd68e5105563bdc09502c7
TJ <77421405+TJ-Blinn@users.noreply.github.com>
2022-03-03 22:50:48 -0500

    add component to hold text inside speech bubbles for Reviews section of AboutPage, and styled

---
+++ src/Components/ReviewItem.js
@@ -0,0 +1,35 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+function ReviewItem({ text }) {
+  return (
+    <ReviewItemStyled>
+      <p>{text}</p>
+    </ReviewItemStyled>
+  );
+}
+
+const ReviewItemStyled = styled.div`
+  padding: 2rem 1rem;
+  border-left: 6px solid var(--border-color);
+  background-color: var(--background-dark-grey);
+  position: relative;
+  width: 50%;
+  &:not(:first-child) {
+    margin-left: 1rem;
+  }
+  &::after {
+    content: "";
+    position: absolute;
+    left: 1rem;
+    border-width: 0.8rem;
+    top: 100%;
+    border-style: solid;
+    border-color: var(--background-dark-grey) transparent transparent var(--background-dark-grey);
+  }
+  p {
+    padding: 1rem 0;
+  }
+`;
+
+export default ReviewItem;

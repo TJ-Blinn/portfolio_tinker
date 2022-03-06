ffdd137afa0c8a07c2d88e5a8563ed35daeec1ed
TJ <77421405+TJ-Blinn@users.noreply.github.com>
2022-03-06 16:42:49 -0500

    add link key to obj and inserted placeholder website to demo functionality

---
+++ src/data/blogs.js
@@ -0,0 +1,44 @@
+import blog1 from "../img/blog/blog1.jpg";
+import blog2 from "../img/blog/blog2.jpg";
+import blog3 from "../img/blog/blog3.jpg";
+import blog4 from "../img/blog/blog4.jpg";
+
+const blogs = [
+  {
+    id: 1,
+    title: "How I began my journey into boardgaming",
+    date: "01",
+    month: "April",
+    image: blog1,
+    link: "https://beerism.ca/2021/12/26/a-beerism-guide-to-aging-beer/",
+  },
+
+  {
+    id: 2,
+    title: "When is it time to buy a Kallax for my boardgame collection?",
+    date: "01",
+    month: "April",
+    image: blog2,
+    link: "https://beerism.ca/2021/12/26/a-beerism-guide-to-aging-beer/",
+  },
+
+  {
+    id: 3,
+    title: "How to incorporate your boardgames into your furniture",
+    date: "01",
+    month: "April",
+    image: blog3,
+    link: "https://beerism.ca/2021/12/26/a-beerism-guide-to-aging-beer/",
+  },
+
+  {
+    id: 4,
+    title: "When to admit that you have a boardgame problem",
+    date: "01",
+    month: "April",
+    image: blog4,
+    link: "https://beerism.ca/2021/12/26/a-beerism-guide-to-aging-beer/",
+  },
+];
+
+export default blogs;

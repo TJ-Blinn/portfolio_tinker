e2587c22004edf32883773de70a9b76dda0879b8
TJ <77421405+TJ-Blinn@users.noreply.github.com>
2022-03-06 23:18:22 -0500

    add styling for icons and props with icons

--- src/Components/ContactItem.js
+++ src/Components/ContactItem.js
@@ -1,17 +1,14 @@
 import React from "react";
 import styled from "styled-components";
 
-function ContactItem({ icon, title, contact1, contact2 }) {
+function ContactItem({ title, icon, contact1 }) {
   return (
     <ContactItemStyled>
-      <div className="left-content">
-        <p>{icon}</p>
-      </div>
+      <div className="left-content">{icon}</div>
 
       <div className="right-content">
         <h6>{title}</h6>
         <p>{contact1}</p>
-        <p>{contact2}</p>
       </div>
     </ContactItemStyled>
   );
@@ -20,6 +17,40 @@ function ContactItem({ icon, title, contact1, contact2 }) {
 const ContactItemStyled = styled.div`
   padding: 1.5rem 2rem;
   background-color: var(--background-dark-grey);
+  display: flex;
+  align-items: center;
+
+  /* give margin bottom to all except last child | Not pseudo element */
+  &:not(:last-child) {
+    margin-bottom: 2rem;
+  }
+
+  /* Styling for the icons */
+  .left-content {
+    padding: 1.5rem;
+    border: 1px solid var(--border-color);
+    font-size: 2rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-right: 1.5rem;
+    svg {
+      font-size: 2.3rem;
+    }
+  }
+
+  .right-content {
+    h6 {
+      color: var(--white-color);
+      font-size: 1.2rem;
+      padding-bottom: 0.6rem;
+    }
+    /* !!! Change these to anchor tags if you want to redirect to email on click !!!  */
+    p {
+      padding: 0.1rem 0;
+      color: var(--primary-color);
+    }
+  }
 `;
 
 export default ContactItem;

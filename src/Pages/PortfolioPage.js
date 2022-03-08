3884cc131e4493d7c30f9dc6a66b2f9bf67af418
TJ <77421405+TJ-Blinn@users.noreply.github.com>
2022-03-08 00:06:27 -0500

    add base filter function targeting categories, add button STATE, add new var for allButtons

--- src/Pages/PortfolioPage.js
+++ src/Pages/PortfolioPage.js
@@ -3,14 +3,24 @@ import Title from "../Components/Title";
 import { MainLayout, InnerLayout } from "../styles/layouts";
 import portfolios from "../data/portfolios";
 import Menu from "../Components/Menu";
+import Button from "../Components/Button";
+
+const allButtons = ["All", "Animation", ...portfolios.map((item) => item.category)];
 
 function PortfolioPage() {
-  const [menuItem, setmenuItem] = useState(portfolios);
+  const [menuItem, setMenuItem] = useState(portfolios);
+  const [button, setButtons] = useState(allButtons);
+
+  const filter = (button) => {
+    const filteredData = portfolios.filter((item) => item.category === button);
+    setMenuItem(filteredData);
+  };
 
   return (
     <MainLayout>
       <Title title={"Portfolios"} span={"portfolios"} />
       <InnerLayout>
+        <Button filter={filter} button={button} />
         <Menu menuItem={menuItem} />
       </InnerLayout>
     </MainLayout>

74fe92ff42fa2f4dce61ee8e0b5890a9172ec012
TJ <77421405+TJ-Blinn@users.noreply.github.com>
2022-03-08 10:34:23 -0500

    add condition to display all categories using ALL button using setMenu state of portfolios data file

--- src/Pages/PortfolioPage.js
+++ src/Pages/PortfolioPage.js
@@ -5,13 +5,18 @@ import portfolios from "../data/portfolios";
 import Menu from "../Components/Menu";
 import Button from "../Components/Button";
 
-const allButtons = ["All", "Animation", ...portfolios.map((item) => item.category)];
+const allButtons = ["All", ...new Set(portfolios.map((item) => item.category))];
 
 function PortfolioPage() {
   const [menuItem, setMenuItem] = useState(portfolios);
   const [button, setButtons] = useState(allButtons);
 
   const filter = (button) => {
+    if (button === "All") {
+      setMenuItem(portfolios);
+      return;
+    }
+
     const filteredData = portfolios.filter((item) => item.category === button);
     setMenuItem(filteredData);
   };

943e9917cd7b2f2a9f922cfdd7ca8535c464a4b9
TJ <77421405+TJ-Blinn@users.noreply.github.com>
2022-03-07 23:20:30 -0500

    add base render with title, innerlayout and menu. Add STATE to this file

--- src/Pages/PortfolioPage.js
+++ src/Pages/PortfolioPage.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Title from "../Components/Title";
 import { MainLayout, InnerLayout } from "../styles/layouts";
+import portfolios from "../data/portfolios";
+import Menu from "../Components/Menu";
 
 function PortfolioPage() {
+  const [menuItem, setmenuItem] = useState(portfolios);
+
   return (
     <MainLayout>
-      <Title title={"Portfolios"} span={"Portfolios"} />
-      <InnerLayout></InnerLayout>
+      <Title title={"Portfolios"} span={"portfolios"} />
+      <InnerLayout>
+        <Menu menuItem={menuItem} />
+      </InnerLayout>
     </MainLayout>
   );
 }

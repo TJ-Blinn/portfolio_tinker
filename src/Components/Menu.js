e9c8c481120bbc4dcd740e202be25103696d3dc2
TJ <77421405+TJ-Blinn@users.noreply.github.com>
2022-03-08 22:12:50 -0500

    styling functional - opacity covers photo, svg icons fly down onhover, colors correct

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -45,6 +45,7 @@ const MenuItemStyled = styled.div`
     .portfolio-content {
       display: block;
       position: relative;
+      overflow: hidden;
       h6 {
         font-size: 1.5rem;
       }
@@ -54,7 +55,92 @@ const MenuItemStyled = styled.div`
         object-fit: cover;
       }
       ul {
-        display: none;
+        /* display: none; */
+        transform: translateY(-600px);
+        transition: all 0.4s ease-in-out;
+        position: absolute;
+        left: 50%;
+        top: 40%;
+        opacity: 0;
+
+        li {
+          background-color: var(--border-color);
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          padding: 1rem;
+          border-radius: 50%;
+          width: 3rem;
+          height: 3rem;
+          margin: 0 0.5rem;
+          transition: all 0.4s ease-in-out;
+          &:hover {
+            background-color: var(--primary-color);
+          }
+
+          a {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            transition: all 0.4s ease-in-out;
+          }
+        }
+      }
+
+      .portfolio-image {
+        &::before {
+          content: "";
+          position: absolute;
+          left: 2%;
+          top: 4%;
+          height: 0;
+          width: 0;
+          transition: all 0.4s ease-in-out;
+        }
+      }
+      .portfolio-image:hover {
+        ul {
+          transform: translateY(0);
+          transform: translate(-50%, -50%);
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          transition: all 0.4s ease-in-out;
+          opacity: 1;
+
+          li {
+            transition: all 0.4s ease-in-out;
+            &:hover {
+              background-color: var(--primary-color);
+            }
+
+            a {
+              display: flex;
+              align-items: center;
+              justify-content: center;
+              transition: all 0.4s ease-in-out;
+            }
+          }
+
+          li:hover {
+            svg {
+              color: var(--white-color);
+            }
+          }
+
+          svg {
+            font-size: 2rem;
+          }
+        }
+
+        &::before {
+          height: calc(100% - 32%);
+          width: calc(100% - 4%);
+          background-color: white;
+          opacity: 0.9;
+          transform-origin: left;
+          transition: all 0.4s ease-in-out;
+        }
       }
     }
   }

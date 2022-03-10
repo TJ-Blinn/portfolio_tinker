da1a4cfe121f180a13108e4e33e5a77135d8d6fa
TJ <77421405+TJ-Blinn@users.noreply.github.com>
2022-03-09 21:59:37 -0500

    add 2 media queries to reduce columns for menu items down to 2 then 1 column

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -41,6 +41,13 @@ const MenuItemStyled = styled.div`
   grid-template-columns: repeat(3, 1fr);
   grid-gap: 2rem;
 
+  @media screen and (max-width: 920px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+  @media screen and (max-width: 670px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
+
   .grid-item {
     .portfolio-content {
       display: block;
